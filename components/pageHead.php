e3e24cf9ba4bb0336897bec396b0187b8df39a4a
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-12-01 17:25:04 +0100

    Milestone 1 completata

---
+++ components/pageHead.php
@@ -0,0 +1,11 @@
+<head>
+  <?php
+  // "importo" i metadati che vanno nella prima parte del tag <head>
+  include_once __DIR__ . "/pageMeta.php";
+  ?>
+
+  <title>PHP Ajax Dischi</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/style.css">
+
+</head>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+// "importo" subito l'array di dati
+require_once __DIR__ . "/data/data.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php
+// "importo" il tag <head> della pagina (facilmente replicabile su altre pg.) pb-card
+require_once __DIR__ . "/components/pageHead.php"
+?>
+
+<body>
+
+  <header class="bg-card container-fluid">
+    <img src="assets/spotify-logo.png" alt="logo">
+  </header>
+
+  <main class="bg-main p-5">
+    <div class="container">
+      <div class="row row-cols-5 g-4">
+        <?php
+        foreach ($cdList as $cd) {
+
+          $poster = $cd["poster"];
+          $title = $cd["title"];
+          $author = $cd["author"];
+          $year = $cd["year"];
+
+          echo "<div class='col'>
+                  <div class='my-card bg-card card border-0 p-3 rounded-0'>
+                    <img src='$poster' class='card-img-top rounded-0' alt='$title'/>
+                    <div class='card-body text-center'>
+                      <h5 class='text-white m-0 pb-3'>$title</h5>
+                      <h6 class='text-secondary m-0 pb-2'>$author</h6>
+                      <p class='text-secondary m-0'>$year</p>
+                    </div>
+                  </div>
+                </div>";
+        }
+        ?>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
